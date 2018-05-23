696a551c114825de0db8b8d5f8701e7f3c5e86c9
Karlo <karlotvrdinic@gmail.com>
2018-05-23 11:08:44 +0200

    add GUI

---
+++ Assets/GUIv2/Script/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public Transform currentMount;
+    public float speedFactor = 0.1f;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, currentMount.position, speedFactor);
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, speedFactor);
+	}
+
+    public void setPosition(Transform newMount)
+    {
+        currentMount = newMount;
+    }
+}
---
+++ Assets/GUIv2/Script/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void continueButton()
+    {
+        Debug.Log("Igra se nastavlja");
+    }
+
+    public void newGameButton()
+    {
+        SceneManager.LoadScene("Intro");
+    }
+
+    public void areYouSureYesButton()
+    {
+        Application.Quit();
+    }
+
+    public void playSound()
+    {
+
+    }
+
+
+}
